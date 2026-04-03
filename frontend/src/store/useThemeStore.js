af214a7bd37e5017a989ed701f412f38b96bee84
ashishxdev <ashishrana2004@gmail.com>
2026-04-03 17:19:29 +0530

    default theme light

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { create } from "zustand"
 
 export const useThemeStore = create((set) => ({
-    theme: localStorage.getItem("connectly-theme") || "Halloween",
+    theme: localStorage.getItem("connectly-theme") || "Light",
     setTheme: (theme) => {
         localStorage.setItem("connectly-theme", theme)
         set({ theme })
